cabbdaca94806ceabec49eeae8c139b1b1bef84b
Thierry <ospital.thierry@neuf.fr>
2018-10-26 00:10:20 +0200

    dernier push

---
+++ db/migrate/20181025141450_create_dogsitters.rb
@@ -0,0 +1,9 @@
+class CreateDogsitters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogsitters do |t|
+      t.string :user_name
+      t.references :city, foreign_key: true
+      t.timestamps
+    end
+  end
+end
